ff9566ea2ad53084fd9fbe1fbd3f19f4f2690e38
KOCHOW <sunny.place.house@gmail.com>
2018-03-14 15:17:56 +0900

    initial commit

---
+++ build/beginner_tutorials/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/my_ros/src/beginner_tutorials
+# Build directory: /mnt/c/Users/Public/my_ros/build/beginner_tutorials
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
