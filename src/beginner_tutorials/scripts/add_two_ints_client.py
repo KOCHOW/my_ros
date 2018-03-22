2a41b50e6282aef92d570a77d444ef3009c9b569
kochow <sunny.place.house@gmail.com>
2018-03-22 19:14:57 +0900

    tutorial_python

--- src/beginner_tutorials/scripts/add_two_ints_client.py
+++ src/beginner_tutorials/scripts/add_two_ints_client.py
@@ -23,5 +23,5 @@ if __name__ == "__main__":
     else:
         print usage()
         sys.exit(1)
-    print "Requesting %s+%s"%(x, y)
+    print "Requesting %s + %s"%(x, y)
     print "%s + %s = %s"%(x, y, add_two_ints_client(x, y))
--- src/beginner_tutorials/scripts/add_two_ints_server.py
+++ src/beginner_tutorials/scripts/add_two_ints_server.py
@@ -4,7 +4,7 @@ from beginner_tutorials.srv import *
 import rospy
 
 def handle_add_two_ints(req):
-    print "Returning [%s + %s = %s]"%(req.a, req.b, (req.a + req.b))
+    print "Returning [%s * %s = %s]"%(req.a, req.b, (req.a + req.b))
     return AddTwoIntsResponse(req.a + req.b)
 
 def add_two_ints_server():
