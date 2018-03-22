d2143b0043b1685599b041f38d372a9087932e0f
kochow <sunny.place.house@gmail.com>
2018-03-22 19:20:30 +0900

    add_python_script

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/my_ros/src
-# Build directory: /mnt/c/Users/Public/my_ros/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(beginner_tutorials)
--- build/beginner_tutorials/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/my_ros/src/beginner_tutorials
-# Build directory: /mnt/c/Users/Public/my_ros/build/beginner_tutorials
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
