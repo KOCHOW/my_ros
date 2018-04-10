62e03875db88ad5a1956a828d73eb66e8b774247
kochow <sunny.place.house@gmail.com>
2018-04-10 12:36:11 +0900

    変更点: 	modified:   pid_ctrl/script/flight_view.py

--- src/pid_ctrl/script/flight_view.py
+++ src/pid_ctrl/script/flight_view.py
@@ -3,29 +3,45 @@
 import rospy
 from pid_ctrl.msg import flight_data
 from std_msgs.msg import String
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as anime
+
 
 key = "s"
-
-def get_flight_data(data):
-    if key != "e":
-        rospy.loginfo(rospy.get_caller_id() + " : t = %f, r = %f, pos = %f, pitch = %f" , data.t, data.r, data.pos, data.pitch)
+data_list = []
+data_size_limit = 1000
+startTime = rospy.Time
+def get_flight_data(data): 
+    global key
+    t = rospy.Time.now() - startTime
+    if key != "e" and len(data_list) < data_size_limit:
+        print(t.secs)
+        #rospy.loginfo(rospy.get_caller_id() + " : t = %f, r = %f, pos = %f, pitch = %f" , data.t, data.r, data.pos, data.pitch)
+        data_list.append([data.t, data.r, data.pos, data.pitch])
+    if len(data_list) == data_size_limit:
+        key = "e"
 
 def finish_callback(data):
     global key
     key = data.data
 
 def listener():
+    global startTime
     rospy.init_node("flight_view", anonymous = True)
     rospy.Subscriber("flight_data", flight_data, get_flight_data)
     rospy.Subscriber("finish", String, finish_callback)
     rate = rospy.Rate(100)
     print("ready")
+    startTime = rospy.Time.now()
     while not rospy.is_shutdown():
         if key == "e":
+            print("終わりだよ～")
             break
         rate.sleep()
         
 
 if __name__ == '__main__':
     listener()
-    print("終わりだよ～")
+    print(data_list)
+    print(len(data_list))
